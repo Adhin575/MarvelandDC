7576147a49d97c7ed26b28df102f86062db90913
Adhin575 <74808892+Adhin575@users.noreply.github.com>
2023-06-01 10:45:40 +0400

    Created posnum.py

---
+++ posnum.py
@@ -0,0 +1,13 @@
+# print positive Numbers in a List
+  
+# input of list
+li=[]
+n=int(input("Enter size of list "))
+for i in range(0,n):
+    e=int(input("Enter element of list "))
+    li.append(e)
+print("Positive numbers in",li,"are: ")
+  
+for i in li:   
+    if i >= 0:
+       print(i, end = " ")
